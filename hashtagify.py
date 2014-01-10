ab54c06f74e50993633c9a26bd3f36cb94aaa79f
Rob Speer <rob@luminoso.com>
2014-01-10 01:56:31 -0500

    support both Python 2 and 3

--- hashtagify.py
+++ hashtagify.py
@@ -1,9 +1,35 @@
+from __future__ import unicode_literals
 import sys
 
+
 def hashtagify(text):
+    """
+    Convert text with spaces to a 'hashtag' without spaces.
+    """
     return '#' + text.lower().replace(' ', '').replace("'", '')
 
-if __name__ == '__main__':
+
+def py2_main():
+    """
+    Convert text with spaces on standard input, to text without spaces on
+    standard output. This version runs on Python 2.
+    """
     for line in sys.stdin:
-        line = line.decode('utf-8').strip()
-        print(hashtagify(line).encode('utf-8'))
+        line = line.decode('utf-8')
+        print(hashtagify(line.strip()).encode('utf-8'))
+
+
+def py3_main():
+    """
+    Convert text with spaces on standard input, to text without spaces on
+    standard output. This version runs on Python 3.
+    """
+    for line in sys.stdin:
+        print(hashtagify(line.strip()))
+
+
+if __name__ == '__main__':
+    if sys.version_info[0] == 2:
+        py2_main()
+    else:
+        py3_main()
